e5a03c129b53fe940ac3a06a8e5dec23a4b1ea50
chokora1028 <masayamasato1028@gmail.com>
2026-08-23 23:44:21 +0900

    タブバーのリンク繋ぐ

--- componets/TabBar.tsx
+++ componets/TabBar.tsx
@@ -6,7 +6,7 @@ export default function TabBar() {
     <nav className="fixed bottom-4 left-1/2 -translate-x-1/2 w-[calc(100%-32px)] max-w-[398px] bg-white/90 backdrop-blur border border-gray-100 rounded-full shadow-lg h-14 flex items-center justify-around z-50 px-2">
       
       {/* 1. マップボタン */}
-      <Link href="/map" className="flex flex-col items-center text-[10px] text-[#c8161d] font-bold no-underline">
+      <Link href="/screen/map" className="flex flex-col items-center text-[10px] text-[#c8161d] font-bold no-underline">
         <span className="text-base">📍</span>
         <span>マップ</span>
       </Link>
@@ -18,7 +18,7 @@ export default function TabBar() {
       </Link>
 
       {/* 3. マイページボタン */}
-      <Link href="/mypage" className="flex flex-col items-center text-[10px] text-gray-400 no-underline">
+      <Link href="/screen/mypage" className="flex flex-col items-center text-[10px] text-gray-400 no-underline">
         <span className="text-base">👤</span>
         <span>マイページ</span>
       </Link>
